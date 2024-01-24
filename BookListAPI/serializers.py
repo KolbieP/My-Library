8c50c9098ca65a2fa5c5c5ed5ddc86f2e24bbb12
Kolbie <kolbierparker20@outlook.com>
2024-01-24 11:29:25 -0600

    Little Spelling Fix

--- BookListAPI/serializers.py
+++ BookListAPI/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Book
 from rest_framework.validators import UniqueValidator
 
-#This serializer has some basic validation in the inputs taht only allows rating to be less than or equal to 5
+#This serializer has some basic validation in the inputs that only allows rating to be less than or equal to 5
 #and for title to be a unique value 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
